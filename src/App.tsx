32fd5c76158ae521a0b2b24fbc5eaa1a7c6f3be8
Yiannis Galanakis <igalas@hotmail.co.uk>
2024-05-01 17:17:09 +0100

    api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+type resultProps = {
+  codeword: number;
+  id: string;
+};
+
+export default function App() {
+  const [result, setResult] = useState<resultProps[]>([]);
+
+  const api = async () => {
+    const data = await fetch("/get-actions", {
+      method: "GET"
+    });
+    const jsonData = await data.json();
+    setResult(jsonData.result);
+  };
+
+  useEffect(() => {
+    api();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+      <button onClick={api}>get-actions</button>
+        {result.map((value) => {
+          return (
+            <div>
+              <div>{value.codeword}</div>
+              <div>{value.id}</div>
+            </div>
+          );
+        })}
+      </h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
-
-export default App;
